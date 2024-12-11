0f2220a90b5a085aff7d22d591fdd32d69bb2425
aaartur23 <artur.solsona.2018@gmail.com>
2024-12-11 12:47:39 +0100

    3enRayaLastUpdate
    Añado revancha

--- src/Juego.java
+++ src/Juego.java
@@ -1,137 +1,148 @@
-import java.util.Random;
-import java.util.Scanner;
-public class Juego {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		Random rn = new Random();
-		
-		String nplayer1 = " ";
-		String nplayer2 = " ";
-		
-		char player1 = 'X';
-		char player2 = 'O';
-		char espacio = '-';
+	import java.util.Random;
+	import java.util.Scanner;
+	public class Juego {
+		public static void main(String[] args) {
 
-		String option = " ";
-		boolean turnoJugador1;
-		boolean juegoActivo = true;
-		char tablero[][] = new char[3][3];
-		String ganador;
-		
-		System.out.println("................................");
-		System.out.println(":Bienvenido a 3 en raya en JAVA:");
-		System.out.println(":..............................:");
-		System.out.println();
-		
-		do {
-			
-			System.out.println("Vais a querer jugar? Si / No : ");
-			option = sc.next().toLowerCase();
-			
-			if(option.equals("si")) {
-				System.out.println("Empecemos !");
+			Scanner sc = new Scanner(System.in);
+			Random rn = new Random();
 
-				System.out.print("Introduce el nombre del Jugador 1: ");
-				nplayer1 = sc.next();
-				System.out.print("Introduce el nombre del Jugador 2: ");
-				nplayer2 = sc.next();
+			String nplayer1 = " ";
+			String nplayer2 = " ";
 
-				// Inicializo el tablero
-				for (int i = 0; i < 3; i++) {
-					for (int j = 0; j < 3; j++) {
-						tablero[i][j] = espacio;
-					}
-				}
+			char player1 = 'X';
+			char player2 = 'O';
+			char espacio = '-';
 
-				// Lanzo moneda para determinar el primer turno
-				turnoJugador1 = rn.nextBoolean();
-				System.out.println("\n¡El sorteo ha determinado que comienza " +
-						(turnoJugador1 ? nplayer1 : nplayer2) + "!\n");
+			String option = " ";
+			boolean turnoJugador1;
+			boolean juegoActivo = true;
+			char tablero[][] = new char[3][3];
+			String ganador;
 
-				while (juegoActivo) {
-					// Mostrar tablero
-					System.out.println("Tablero actual:");
+			System.out.println("................................");
+			System.out.println(":Bienvenido a 3 en raya en JAVA:");
+			System.out.println(":..............................:");
+			System.out.println();
+
+			do {
+
+				System.out.println("Vais a querer jugar? Si / No : ");
+				option = sc.next().toLowerCase();
+
+				if(option.equals("si")) {
+					System.out.println("Empecemos !");
+
+					System.out.print("Introduce el nombre del Jugador 1: ");
+					nplayer1 = sc.next();
+					System.out.print("Introduce el nombre del Jugador 2: ");
+					nplayer2 = sc.next();
+
+					// Inicializo el tablero
 					for (int i = 0; i < 3; i++) {
 						for (int j = 0; j < 3; j++) {
-							System.out.print(tablero[i][j] + " ");
+							tablero[i][j] = espacio;
 						}
-						System.out.println();
 					}
 
-					// Turno del jugador actual
-					System.out.println((turnoJugador1 ? nplayer1 : nplayer2) +
-							", es tu turno. Introduce fila y columna (0-2):");
+					// Lanzo moneda para determinar el primer turno
+					turnoJugador1 = rn.nextBoolean();
+					System.out.println("\n¡El sorteo ha determinado que comienza " +
+							(turnoJugador1 ? nplayer1 : nplayer2) + "!\n");
 
-					int fila = sc.nextInt();
-					int columna = sc.nextInt();
-
-					// Valido movimiento
-					if (fila >= 0 && fila < 3 && columna >= 0 && columna < 3 && tablero[fila][columna] == espacio) {
-						tablero[fila][columna] = turnoJugador1 ? player1 : player2;
-
-						// Compruebo si alguien ha ganado
-						boolean hayGanador = false;
-
-						// Compruebo filas
+					while (juegoActivo) {
+						// Mostrar tablero
+						System.out.println("Tablero actual:");
 						for (int i = 0; i < 3; i++) {
-							if (tablero[i][0] == tablero[i][1] && tablero[i][1] == tablero[i][2] && tablero[i][0] != espacio) {
-								hayGanador = true;
+							for (int j = 0; j < 3; j++) {
+								System.out.print(tablero[i][j] + " ");
 							}
+							System.out.println();
 						}
 
-						// Compruebo columnas
-						for (int i = 0; i < 3; i++) {
-							if (tablero[0][i] == tablero[1][i] && tablero[1][i] == tablero[2][i] && tablero[0][i] != espacio) {
-								hayGanador = true;
-							}
-						}
+						// Turno del jugador actual
+						System.out.println((turnoJugador1 ? nplayer1 : nplayer2) +
+								", es tu turno. Introduce fila y columna (0-2):");
 
-						// Compruebo diagonales
-						if (tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2] && tablero[0][0] != espacio) {
-							hayGanador = true;
-						}
-						if (tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0] && tablero[0][2] != espacio) {
-							hayGanador = true;
-						}
+						int fila = sc.nextInt();
+						int columna = sc.nextInt();
 
-						if (hayGanador) {
-							juegoActivo = false;
-							ganador = turnoJugador1 ? nplayer1 : nplayer2;
-							System.out.println("¡" + ganador + " ha ganado!");
-						} else {
-							// Compruebo empate
-							boolean empate = true;
+						// Valido movimiento
+						if (fila >= 0 && fila < 3 && columna >= 0 && columna < 3 && tablero[fila][columna] == espacio) {
+							tablero[fila][columna] = turnoJugador1 ? player1 : player2;
+
+							// Compruebo si alguien ha ganado
+							boolean hayGanador = false;
+
+							// Compruebo filas
 							for (int i = 0; i < 3; i++) {
-								for (int j = 0; j < 3; j++) {
-									if (tablero[i][j] == espacio) {
-										empate = false;
-									}
+								if (tablero[i][0] == tablero[i][1] && tablero[i][1] == tablero[i][2] && tablero[i][0] != espacio) {
+									hayGanador = true;
 								}
 							}
-							if (empate) {
-								juegoActivo = false;
-								System.out.println("¡Es un empate!");
-							}
-						}
 
-						// Cambio turno
-						turnoJugador1 = !turnoJugador1;
-					} else {
-						System.out.println("Movimiento inválido. Intenta de nuevo.");
+							// Compruebo columnas
+							for (int i = 0; i < 3; i++) {
+								if (tablero[0][i] == tablero[1][i] && tablero[1][i] == tablero[2][i] && tablero[0][i] != espacio) {
+									hayGanador = true;
+								}
+							}
+
+							// Compruebo diagonales
+							if (tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2] && tablero[0][0] != espacio) {
+								hayGanador = true;
+							}
+							if (tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0] && tablero[0][2] != espacio) {
+								hayGanador = true;
+							}
+
+							if (hayGanador) {
+								juegoActivo = false;
+								ganador = turnoJugador1 ? nplayer1 : nplayer2;
+								System.out.println("¡" + ganador + " ha ganado!");
+							} else {
+								// Compruebo empate
+								boolean empate = true;
+								for (int i = 0; i < 3; i++) {
+									for (int j = 0; j < 3; j++) {
+										if (tablero[i][j] == espacio) {
+											empate = false;
+										}
+									}
+								}
+								if (empate) {
+									juegoActivo = false;
+									System.out.println("¡Es un empate!");
+								}
+							}
+
+							// Cambio turno
+							turnoJugador1 = !turnoJugador1;
+						} else {
+							System.out.println("Movimiento inválido. Intenta de nuevo.");
+						}
 					}
+
+					// Pregunto por si quieren revancha
+					System.out.println("¿Quereis jugar otra vez? (Si / No): ");
+					String opcion = sc.next().toLowerCase();
+					if (!opcion.equals("si")) {
+						System.out.println("Gracias por jugar. ¡Adiós!");
+						break;
+					} else {
+						juegoActivo = true;
+						ganador = null;
+					}
+
+						} else if(option.equals("no")) {
+					System.out.println("Has salido correctamente...");
+					break;
+				} else {
+					System.out.println("Opcion no valida");
 				}
 
-					} else if(option.equals("no")) {
-				System.out.println("Has salido correctamente...");
-				break;
-			} else { 
-				System.out.println("Opcion no valida");
-			}
-			
-		}while(option.equals("no"));
-		
-		
-	}
+			}while(option.equals("no"));
 
-}
+
+		}
+
+	}
